63c1a1553638aec8da380d41313d0d94b3244163
Christopher Sang <csdev@users.noreply.github.com>
2015-06-06 16:54:32 -0400

    update import

--- datacheck/__init__.py
+++ datacheck/__init__.py
@@ -2,11 +2,13 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import *
 
-from datacheck.core import validate, Type, List, Required, Optional, Dict
+from datacheck.core import (validate, Validator, Type, List,
+                            Required, Optional, Dict)
 
 
 __all__ = [
     'validate',
+    'Validator',
     'Type',
     'List',
     'Required',
